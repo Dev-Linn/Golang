50538f80dcb22abd922fc53e098d1d734fa1d0da
Lincoln costa Silva - Linn <lincoln.beitsbancarios.angola@gmail.com>
2023-11-03 11:24:24 -0300

    <3

--- 1 - Pacotes/go.mod
+++ 1 - Pacotes/go.mod
@@ -3,3 +3,5 @@ module moodulo
 go 1.21.3
 
 //Centralizar todas dependencias.
+
+require github.com/badoux/checkmail v1.2.1
--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"moodulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Hello word!")
 	auxiliar.Escrever()
-	
+
+	erro := checkmail.ValidateFormat("lincoln@gmail.com")
+	fmt.Println(erro)
+
+	if erro == nil {
+		fmt.Println("O email é válido")
+	} else {
+		fmt.Println("O email é inválido")
+	}
+
 }
