9c01c9cb862be0d53d70e9256961487339a2dbf2
Lincoln costa Silva - Linn <lincoln.beitsbancarios.angola@gmail.com>
2023-11-03 12:28:17 -0300

    <3

---
+++ 2 - Variáveis/var.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var var1 = "Olá mundo!"
+	var2 := "Variavel 2"
+	fmt.Println(var1)
+	fmt.Println(var2)
+
+	const constate1 string = "constante 1"
+	fmt.Println(constate1)
+
+	var5, var6 := "Variavel 6" , "Variavel 7"
+	fmt.Println(var5, var6)
+
+}
