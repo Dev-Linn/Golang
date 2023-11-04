7d819bca91a05da6b8e237431d51f480d6960dca
Lincoln costa Silva - Linn <lincoln.beitsbancarios.angola@gmail.com>
2023-11-04 12:39:28 -0300

    <3

---
+++ 4 - Func/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func somar(n1, n2 int8) int8 {
+	return n1 + n2
+}
+
+func calc(n1, n2 int8) (int8, int8) {
+	adicao := n1 + n2
+	sub := n1 - n2
+
+	return adicao, sub
+}
+
+
+func main() {
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var a = func(txt string) string {
+		fmt.Println(txt)
+
+		return txt
+		
+	}
+
+	retorno := a("Texto da função 1")
+	fmt.Println(retorno)
+
+	// Se eu substituir as variaveis (resultadosSoma, resultadoSub) por _ ele ignora um e da certo.
+	resultadosSoma, resultadoSub := calc(10, 16)
+	fmt.Println(resultadosSoma, resultadoSub)
+
+}
